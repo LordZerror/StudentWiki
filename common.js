77671dcba3a6723db0c7a84ef5f60b49916eff06
Vaibhavi <porevaibhavi5@gmail.com>
2021-10-26 19:46:05 +0530

    Modified UI

---
+++ common.js
@@ -0,0 +1,13 @@
+
+function visib(num){
+    var myDiv=document.createElement('div');
+    myDiv.className='nb';
+    document.getElementsByClassName('LI')[num].appendChild(myDiv);
+}
+
+function invisib(n){
+    var Div=document.getElementsByClassName('LI')[n];
+    Div.removeChild(Div.lastChild);
+}
+
+    
