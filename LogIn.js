5c3ea975c275af728d13f5767d4ecd2bc5c26209
Vaibhavi <porevaibhavi5@gmail.com>
2022-03-10 10:41:40 +0530

    dk

--- LogIn.js
+++ LogIn.js
@@ -2,7 +2,7 @@
 var modal = document.getElementById('id01');
 // When the user clicks anywhere outside of the login page, the login page gets closed
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target == modal) { 
         modal.style.display = "none";
     }
 }
